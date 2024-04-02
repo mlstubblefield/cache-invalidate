28996e7aef09835b1bf8fda0a436cc2d398b36d4
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2024-04-02 16:50:43 -0500

    little more cache invalidation

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -2,5 +2,6 @@ import { revalidatePath } from 'next/cache'
  
 export async function GET(request) {
     revalidatePath('/test')
+    revalidatePath('/example1')
     return Response.json({ revalidated: true, now: Date.now() })
 }
